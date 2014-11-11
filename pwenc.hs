bfa32c0917f1c3ef1dd8903f73602bec8c80f8fc
Joachim Fasting <joachifm@fastmail.fm>
2014-11-11 20:36:05 +0100

    pwenc operates on files only

--- pwenc.hs
+++ pwenc.hs
@@ -10,13 +10,14 @@ import Data.Maybe (fromMaybe)
 import Data.String (fromString)
 import qualified Data.ByteString as SB
 
+import System.Environment (getArgs)
 import qualified System.Console.Haskeline as Line
 
 ------------------------------------------------------------------------
 -- Command wrappers
 
-cmdEnc :: SB.ByteString
-       -> SB.ByteString
+cmdEnc :: SB.ByteString -- ^ Passphrase
+       -> SB.ByteString -- ^ Plaintext
        -> IO (SB.ByteString, Int, SB.ByteString)
 cmdEnc pass plain = do
   salt <- getSalt
@@ -26,13 +27,25 @@ cmdEnc pass plain = do
 ------------------------------------------------------------------------
 -- Command-line interface.
 
-getFileContents "-" = SB.getContents
-getFileContents fn  = SB.readFile fn
-
-getPassword "" = Line.runInputT Line.defaultSettings $
-  fromString . fromMaybe "" <$> Line.getPassword (Just '*') "Password: "
-getPassword fn = SB.readFile fn
+getPassword :: IO SB.ByteString
+getPassword = Line.runInputT Line.defaultSettings loop
+  where
+    input p = fromString. fromMaybe "" <$> Line.getPassword (Just '*') p
+    loop = do
+      pw1 <- input "Enter passphrase: "
+      pw2 <- input "Verify          : "
+      if pw1 == pw2
+        then return pw1
+        else Line.outputStrLn "Mismatch" >> loop
 
 main :: IO ()
-main = print =<< uncurry cmdEnc =<<
-  ((,) <$> getPassword "" <*> getFileContents "-")
+main = do
+  args <- getArgs
+  case args of
+    [inFile, outFile] -> do
+      pw <- getPassword
+      cs <- SB.readFile inFile
+      es <- cmdEnc pw cs
+      SB.writeFile outFile (fromString $ show es)
+    _ -> error "Missing arguments\nUsage: pwenc <infile> <outfile>"
+    
